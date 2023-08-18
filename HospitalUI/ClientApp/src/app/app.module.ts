084d8d2d11890088dab0642f742a9927113f4bdd
Hussein Issa <www.hussein2001@gmail.com>
2023-08-18 23:11:02 +0300

    Delete the default pages in the frontend and backend project

--- HospitalUI/ClientApp/src/app/app.module.ts
+++ HospitalUI/ClientApp/src/app/app.module.ts
@@ -1,35 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
-
 import { AppComponent } from './app.component';
 import { NavMenuComponent } from './nav-menu/nav-menu.component';
 import { HomeComponent } from './home/home.component';
-import { CounterComponent } from './counter/counter.component';
-import { FetchDataComponent } from './fetch-data/fetch-data.component';
 import { ModalModule } from 'ngx-bootstrap/modal';
 import { SharedModule } from './shared/shared.module';
-import { PatientModule } from './patient/patient.module';
-import { AddPatientComponent } from './patient/add-update-patient-model/add-patient/add-patient.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavMenuComponent,
     HomeComponent,
-    CounterComponent,
-    FetchDataComponent,
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
     HttpClientModule,
     RouterModule.forRoot([
-      { path: 'counter', component: CounterComponent },
       { path: 'home', component: HomeComponent},
-      { path: 'fetch-data', component: FetchDataComponent },
       {
         path: 'patients',
         loadChildren: () =>
